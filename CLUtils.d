a542c7a006060af3835323ff4df6137c023f1ab1
Ben Grabham <bsg110@ic.ac.uk>
2012-10-15 12:00:53 +0100

    Updated CLUtils to store the platforms and devices in case we want them

--- CLUtils.d
+++ CLUtils.d
@@ -8,29 +8,31 @@ struct CLInfo {
   CLDevice device;
   CLContext context;
   CLCommandQueue queue;
+  typeof(CLHost.getPlatforms()) platforms;
+  typeof(platform.allDevices()) devices;
 }
 
 auto InitCL() {
   CLInfo info;
-  auto platforms = CLHost.getPlatforms();
+  info.platforms = CLHost.getPlatforms();
 
-  if (platforms.length < 1)
+  if (info.platforms.length < 1)
     throw new CLException(CL_INVALID_PLATFORM, "No platforms available.");
 
-  debug foreach(i, platform; platforms)
-      writefln("[%s] %s\n\t%s\n\t%s\n\t%s\n\t%s", i == 0 ? "*" : " ", platform.name, platform.vendor, platform.clversion, platform.profile, platform.extensions);
+  debug foreach(i, platform; info.platforms)
+    writefln("[%s] %s\n\t%s\n\t%s\n\t%s\n\t%s", i == 0 ? "*" : " ", platform.name, platform.vendor, platform.clversion, platform.profile, platform.extensions);
 
-  info.platform = platforms[0];
-  auto devices = info.platform.allDevices;
+  info.platform = info.platforms[0];
+  info.devices = info.platform.allDevices;
 
-  if (devices.length < 1)
+  if (info.devices.length < 1)
     throw new CLException(CL_INVALID_DEVICE, "No devices available.");
 
   debug foreach(i, device; devices)
     writefln("[%s] %s\n\t%s\n\t%s\n\t%s\n\t%s", i == 0 ? "*" : " ", device.name, device.vendor, device.driverVersion, device.clVersion, device.profile, device.extensions);
 
-  info.context = CLContext(devices);
-  info.device = devices[0];
+  info.context = CLContext(info.devices);
+  info.device = info.devices[0];
 
   // Create a command queue and use the first device
   info.queue = CLCommandQueue(info.context, info.device);
