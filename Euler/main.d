b9df5f85b6f108211ab9e1da13804f6f810c8972
Ben Grabham <bsg110@ic.ac.uk>
2012-10-15 12:14:12 +0100

    Correct spacing

--- Euler/main.d
+++ Euler/main.d
@@ -7,14 +7,14 @@ import std.string;
 import Euler.Problems;
 
 void euler(CLInfo info) {
-    writefln("\nPlease select a problem:");
+  writefln("\nPlease select a problem:");
 
-    foreach(i, problem; problems)
-      writefln("%d. %s", i, problem.name);
+  foreach(i, problem; problems)
+    writefln("%d. %s", i, problem.name);
 
-    write("\n=> ");
-    int num = to!int(readln().strip());
+  write("\n=> ");
+  int num = to!int(readln().strip());
 
-    if(num in problems)
-      problems[num].run(info);
+  if(num in problems)
+    problems[num].run(info);
 }
